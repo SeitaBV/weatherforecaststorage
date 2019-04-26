2453bf6debe2b386d8f2a659d6b83588a9105ac1
Nicolas Höning <nicolas@seita.nl>
2019-04-26 15:57:38 +0200

    refactor hourly task so we can differntiate between file and db persistence. Db persistence now needs implementation.

--- weatherforecast/hourly_update_task.py
+++ weatherforecast/hourly_update_task.py
@@ -21,16 +21,25 @@ def forecast_is_new(historical_forecast: pd.DataFrame, event_start: str, source:
     return temp.shape[0] == 0
 
 
-def update_forecast(historical_forecast: pd.DataFrame, current_forecast: pd.DataFrame) -> pd.DataFrame:
+def filter_out_known_forecast(current_forecasts: pd.DataFrame, historical_forecasts: pd.DataFrame) -> pd.DataFrame:
     new_entries_list = []
-    for forecast in current_forecast.itertuples():
+    for forecast in current_forecasts.itertuples():
         event_start: str = forecast[1]
         belief_time: str = forecast[2]
         source: int = forecast[3]
         sensor_id: str = forecast[4]
         event_value: float = forecast[5]
 
-        if forecast_is_new(historical_forecast, event_start, source, sensor_id, event_value):
+        persistence_type = get_config("PERSISTENCE", "type")
+        if persistence_type == "file":
+            is_new = forecast_is_new(historical_forecasts, event_start, source, sensor_id, event_value)
+        elif persistence_type == "db":
+            print("TODO: implement db lookup")
+            is_new = False
+        else:
+            raise Exception("Unkown persistence type: %s" % persistence_type)
+
+        if is_new:
             logging.debug('Adding new entry for %s' % event_start)
             new_entries_list.append({
                 'event_start': event_start,
@@ -40,43 +49,48 @@ def update_forecast(historical_forecast: pd.DataFrame, current_forecast: pd.Data
                 'event_value': event_value
             })
 
-    return pd.DataFrame(new_entries_list, columns=current_forecast.columns)
+    return pd.DataFrame(new_entries_list, columns=current_forecasts.columns)
+
+
+def save_forecasts(new_entries: pd.DataFrame, historical_forecasts: pd.DataFrame) -> pd.DataFrame:
+    if new_entries.shape[0] > 0:
+        logging.info('Adding {} new entries.'.format(new_entries.shape[0]))
+        persistence_type = get_config("PERSISTENCE", "type")
+        if persistence_type == "file":
+            historical_forecasts = historical_forecasts.append(new_entries, ignore_index=True, sort=True)
+            historical_forecasts.to_csv("%s/%s" % (path_to_data(), get_config("PERSISTENCE", "name")), index=False, columns=new_entries.columns)
+        elif persistence_type == "db":
+            print("TODO: implement db saving")
+        else:
+            raise Exception("Unkown persistence type: %s" % persistence_type)
+    else:
+        logging.info('No new entries.')
+    return historical_forecasts
+
 
 
 if __name__ == '__main__':
     """
-    TODO: Two ways of configuring where forecasts are:
-    csv or DB (first gets a filename, second a connection string).
-    This affects also the forecast_is_new function (can become two functions),
-    update_forecast can be called filter_out_known_forecasts
-    and I suggest we make a new function for saving forecasts (to either CSV or DB).
     """
     logging.basicConfig(level=logging.INFO)
     sensors = Sensor.ALL.value
 
-    logging.info("Reading in existing forecasts ...")
-    if os.path.exists('%s/forecasts.csv' % path_to_data()):
-        historical_forecast = pd.read_csv('%s/forecasts.csv' % path_to_data())
-    else:
-        historical_forecast = pd.DataFrame()
+    # reading historical forecasts once, which we will need for file-based storage
+    historical_forecasts = pd.DataFrame()
+    if get_config("PERSISTENCE", "type") == "file":
+        logging.info("Reading in existing forecasts ...")
+        if os.path.exists('%s/forecasts.csv' % path_to_data()):
+            historical_forecasts = pd.read_csv('%s/forecasts.csv' % path_to_data())
 
     for forecast_location in get_config("LOCATIONS"):
         city, country = [s.strip() for s in forecast_location.split(",")]
-     
         logging.info("Getting weather forecasts for %s (%s) ..." % (city, country))
 
-        city_locations = location_utility.get_city_location(city, country)
-
         logging.info("Asking DarkSky for forecasts and finding out which ones are novel ...")
-        current_forecast = create_forecast_table(city_locations, sensors)
+        city_locations = location_utility.get_city_location(city, country)
+        current_forecasts = create_forecast_table(city_locations, sensors, 12)
+        new_entries = filter_out_known_forecast(current_forecasts, historical_forecasts)
 
-        new_entries = update_forecast(historical_forecast, current_forecast)
-
-        if new_entries.shape[0] > 0:
-            logging.info('Adding {} new entries.'.format(new_entries.shape[0]))
-            historical_forecast = historical_forecast.append(new_entries, ignore_index=True, sort=True)
-            historical_forecast.to_csv('%s/forecasts.csv' % path_to_data(), index=False, columns=current_forecast.columns)
-        else:
-            logging.info('No new entries.')
-
+        # TODO: this overwrites older locations when using file persistence
+        historical_forecasts = save_forecasts(new_entries, historical_forecasts)
 
